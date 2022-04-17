7c6e1a4d7101361835668c0dc446a99ff0d9d52e
clebson.araujo <clebson.araujo@l5.com.br>
2022-04-17 18:06:43 -0300

    Instalação Cors

--- app.js
+++ app.js
@@ -4,17 +4,26 @@ const clientRoute = require("./routes/client.route.js");
 const serviceRoute = require("./routes/service.route.js");
 const userRouter = require("./routes/user.route.js");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use("/clients", clientRoute);
 app.use("/services", serviceRoute);
 app.use("/users", userRouter);
-// app.use("/users", userRouter);
 
 app.use((err, req, res, next) => {
-    res.status(400).send({error: err.message});
+  res.status(400).send({ error: err.message });
 });
 
 module.exports = app;
