3545f8a1a52afd08582479beead91b5b384f26dd
clebson.araujo <clebson.araujo@l5.com.br>
2022-09-09 19:21:59 -0300

    Criação dos trycatch para arquivos faltantes

--- model/client.model.js
+++ model/client.model.js
@@ -8,33 +8,37 @@ const mysql = require("./mysql.js").pool;
 function insertClient(client) {
   // console.log('client.model insertClient client', client);
   return new Promise((resolve, reject) => {
-    const sql =
-      "INSERT INTO clientes (nomecliente, dtnasccliente, dtcadcliente, email, whatsapp, enderecocliente, numeroendcliente, bairrocliente, cepcliente, cidadecliente) VALUES (?,?,?,?,?,?,?,?,?,?)";
-    const values = [
-      client.nomecliente,
-      client.dtnasccliente,
-      client.dtcadcliente,
-      client.email,
-      client.whatsapp,
-      client.enderecocliente,
-      client.numeroendcliente,
-      client.bairrocliente,
-      client.cepcliente,
-      client.cidadecliente,
-    ];
-    // console.log('client.model insertClient values', values);
-    mysql.getConnection((err, conn) => {
-      conn.query(sql, values, (err, result, field) => {
-        // console.log('client.model insertClient result', result);
-        console.log('client.model insertClient conn.query err', err);
-        conn.release();
-        if (err) {
-          reject(err);
-          return;
-        }
-        resolve(result);
+    try {
+      const sql =
+        "INSERT INTO clientes (nomecliente, dtnasccliente, dtcadcliente, email, whatsapp, enderecocliente, numeroendcliente, bairrocliente, cepcliente, cidadecliente) VALUES (?,?,?,?,?,?,?,?,?,?)";
+      const values = [
+        client.nomecliente,
+        client.dtnasccliente,
+        client.dtcadcliente,
+        client.email,
+        client.whatsapp,
+        client.enderecocliente,
+        client.numeroendcliente,
+        client.bairrocliente,
+        client.cepcliente,
+        client.cidadecliente,
+      ];
+      // console.log('client.model insertClient values', values);
+      mysql.getConnection((err, conn) => {
+        conn.query(sql, values, (err, result, field) => {
+          // console.log('client.model insertClient result', result);
+          console.log('client.model insertClient conn.query err', err);
+          conn.release();
+          if (err) {
+            reject(err);
+            return;
+          }
+          resolve(result);
+        });
       });
-    });
+    } catch (err) {
+      console.log('client.model insertClient catch err', err);
+    }
   });
 }
 
@@ -74,48 +78,52 @@ function getClients() {
 function getClient(id) {
   // console.log('client.model getClient id', id);
   return new Promise((resolve, reject) => {
-    let sql = `SELECT * FROM clientes WHERE idcliente = ${id}`;
-    let objClient = {};
-    mysql.getConnection((err, conn) => {
-      conn.query(sql, (err, result, field) => {
-        // console.log('client.model getClient result', result);
-        console.log('client.model getClient conn.query err', err);
-        if (err) {
-          reject(err);
-        } else if (result.length == 0) {
-          result = [];
-          resolve(result);
-        } else {
-          objClient = {
-            idcliente: result[0].idcliente,
-            nomecliente: result[0].nomecliente,
-            dtnasccliente: result[0].dtnasccliente,
-            dtcadcliente: result[0].dtcadcliente,
-            email: result[0].email,
-            whatsapp: result[0].whatsapp,
-            enderecocliente: result[0].enderecocliente,
-            numeroendcliente: result[0].numeroendcliente,
-            bairrocliente: result[0].bairrocliente,
-            cepcliente: result[0].cepcliente,
-            cidadecliente: result[0].cidadecliente,
-          };
+    try {
+      let sql = `SELECT * FROM clientes WHERE idcliente = ${id}`;
+      let objClient = {};
+      mysql.getConnection((err, conn) => {
+        conn.query(sql, (err, result, field) => {
+          // console.log('client.model getClient result', result);
+          console.log('client.model getClient conn.query err', err);
+          if (err) {
+            reject(err);
+          } else if (result.length == 0) {
+            result = [];
+            resolve(result);
+          } else {
+            objClient = {
+              idcliente: result[0].idcliente,
+              nomecliente: result[0].nomecliente,
+              dtnasccliente: result[0].dtnasccliente,
+              dtcadcliente: result[0].dtcadcliente,
+              email: result[0].email,
+              whatsapp: result[0].whatsapp,
+              enderecocliente: result[0].enderecocliente,
+              numeroendcliente: result[0].numeroendcliente,
+              bairrocliente: result[0].bairrocliente,
+              cepcliente: result[0].cepcliente,
+              cidadecliente: result[0].cidadecliente,
+            };
 
-          // console.log('client.model getClient objClient', objClient);
-          let sql2 = `SELECT idservico, tiposervico, ambienteservico, dtcadservico, dtevento, preco, statusservico, enderecoevento, numeroendevento, bairroevento, cepevento, cidadeevento, nomebebe, dtnascbebe, nomecrianca FROM servicos INNER JOIN clientes ON servicos.idcliente = clientes.idcliente WHERE clientes.idcliente = ${objClient.idcliente}`;
-          conn.query(sql2, (err, result2) => {
-            // console.log('client.model getClient result2', result2);
-            console.log('client.model getClient conn.query #2 err', err);
-            if (err) {
-              reject(err);
-              return;
-            }
-            objClient.servicos = result2;
-            conn.release();
-            resolve(objClient);
-          });
-        }
+            // console.log('client.model getClient objClient', objClient);
+            let sql2 = `SELECT idservico, tiposervico, ambienteservico, dtcadservico, dtevento, preco, statusservico, enderecoevento, numeroendevento, bairroevento, cepevento, cidadeevento, nomebebe, dtnascbebe, nomecrianca FROM servicos INNER JOIN clientes ON servicos.idcliente = clientes.idcliente WHERE clientes.idcliente = ${objClient.idcliente}`;
+            conn.query(sql2, (err, result2) => {
+              // console.log('client.model getClient result2', result2);
+              console.log('client.model getClient conn.query #2 err', err);
+              if (err) {
+                reject(err);
+                return;
+              }
+              objClient.servicos = result2;
+              conn.release();
+              resolve(objClient);
+            });
+          }
+        });
       });
-    });
+    } catch (err) {
+      console.log('client.model getClient catch err', err);
+    }
   });
 }
 
@@ -128,18 +136,22 @@ function getClient(id) {
 function deleteClient(id) {
   // console.log('client.model deleteClient id', id);
   return new Promise((resolve, reject) => {
-    let sql = `DELETE FROM clientes where idcliente = ${id}`;
-    mysql.getConnection((err, conn) => {
-      conn.query(sql, (err, result, field) => {
-        // console.log('client.model deleteClient result', result);
-        console.log('client.model deleteClient conn.query err', err);
-        conn.release();
-        if(err){
-          result = err;
-        }  
-        resolve(result);
+    try {
+      let sql = `DELETE FROM clientes where idcliente = ${id}`;
+      mysql.getConnection((err, conn) => {
+        conn.query(sql, (err, result, field) => {
+          // console.log('client.model deleteClient result', result);
+          console.log('client.model deleteClient conn.query err', err);
+          conn.release();
+          if (err) {
+            result = err;
+          }
+          resolve(result);
+        });
       });
-    });
+    } catch (err) {
+      console.log('client.model deleteClient catch err', err);
+    }
   });
 }
 
@@ -153,7 +165,8 @@ function deleteClient(id) {
 function updateClient(client) {
   // console.log('client.model updateClient client', client);
   return new Promise((resolve, reject) => {
-    let sql = `UPDATE clientes SET 
+    try {
+      let sql = `UPDATE clientes SET 
     nomecliente = '${client.nomecliente}', 
     dtnasccliente = '${client.dtnasccliente}', 
     dtcadcliente = '${client.dtcadcliente}', 
@@ -165,19 +178,22 @@ function updateClient(client) {
     cepcliente = '${client.cepcliente}', 
     cidadecliente = '${client.cidadecliente}' 
     where idcliente = ${client.idcliente}`;
-    // console.log('client.model updateClient sql', sql);
-    mysql.getConnection((err, conn) => {
-      conn.query(sql, (err, result, field) => {
-        // console.log('client.model updateClient result', result);
-        console.log('client.model updateClient conn.query err', err);
-        if (err) {
-          reject(err);
-          return;
-        }
-        resolve(result);
-        conn.release();
+      // console.log('client.model updateClient sql', sql);
+      mysql.getConnection((err, conn) => {
+        conn.query(sql, (err, result, field) => {
+          // console.log('client.model updateClient result', result);
+          console.log('client.model updateClient conn.query err', err);
+          if (err) {
+            reject(err);
+            return;
+          }
+          resolve(result);
+          conn.release();
+        });
       });
-    });
+    } catch (err) {
+      console.log('client.model updateClient catch err', err);
+    }
   });
 }
 
--- model/service.model.js
+++ model/service.model.js
@@ -9,38 +9,42 @@ const mysql = require("./mysql").pool;
 function insertService(service) {
   // console.log('service.model insertService service', service);
   return new Promise((resolve, reject) => {
-    let sql = `INSERT INTO servicos (tiposervico, ambienteservico, dtcadservico, dtevento, preco, statusservico, enderecoevento, numeroendevento, bairroevento, cepevento, cidadeevento, nomebebe, dtnascbebe, nomecrianca, dtnasccrianca, idcliente) 
+    try {
+      let sql = `INSERT INTO servicos (tiposervico, ambienteservico, dtcadservico, dtevento, preco, statusservico, enderecoevento, numeroendevento, bairroevento, cepevento, cidadeevento, nomebebe, dtnascbebe, nomecrianca, dtnasccrianca, idcliente) 
     values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`;
-    let values = [
-      service.tiposervico,
-      service.ambienteservico,
-      service.dtcadservico,
-      service.dtevento,
-      service.preco,
-      service.statusservico,
-      service.enderecoevento,
-      service.numero,
-      service.bairro,
-      service.cep,
-      service.cidade,
-      service.nomebebe,
-      service.dtnascbebe,
-      service.nomecrianca,
-      service.dtnasccrianca,
-      service.idcliente,
-    ];
-    // console.log('service.model insertService values', values);
-    mysql.getConnection((err, conn) => {
-      conn.query(sql, values, (err, result) => {
-        // console.log('service.model insertService result', result);
-        console.log('service.model insertService conn.query err', err);
-        if(err){
-          result = err;
-        }  
-        resolve(result);
-        conn.release();
+      let values = [
+        service.tiposervico,
+        service.ambienteservico,
+        service.dtcadservico,
+        service.dtevento,
+        service.preco,
+        service.statusservico,
+        service.enderecoevento,
+        service.numero,
+        service.bairro,
+        service.cep,
+        service.cidade,
+        service.nomebebe,
+        service.dtnascbebe,
+        service.nomecrianca,
+        service.dtnasccrianca,
+        service.idcliente,
+      ];
+      // console.log('service.model insertService values', values);
+      mysql.getConnection((err, conn) => {
+        conn.query(sql, values, (err, result) => {
+          // console.log('service.model insertService result', result);
+          console.log('service.model insertService conn.query err', err);
+          if (err) {
+            result = err;
+          }
+          resolve(result);
+          conn.release();
+        });
       });
-    });
+    } catch (err) {
+      console.log('client.model insertService catch err', err);
+    }
   });
 }
 
@@ -51,19 +55,23 @@ function insertService(service) {
 function getServices() {
   // console.log('service.model getServices');
   return new Promise((resolve, reject) => {
-    let sql = `SELECT * FROM servicos`;
-    mysql.getConnection((err, conn) => {
-      conn.query(sql, (err, result) => {
-        // console.log('service.model getServices result', result);
-        console.log('service.model getServices conn.query err', err);
-        if (err) {
-          reject(err);
-          return;
-        }
-        resolve(result);
-        conn.release();
+    try {
+      let sql = `SELECT * FROM servicos`;
+      mysql.getConnection((err, conn) => {
+        conn.query(sql, (err, result) => {
+          // console.log('service.model getServices result', result);
+          console.log('service.model getServices conn.query err', err);
+          if (err) {
+            reject(err);
+            return;
+          }
+          resolve(result);
+          conn.release();
+        });
       });
-    });
+    } catch (err) {
+      console.log('client.model getServices catch err', err);
+    }
   });
 }
 
@@ -75,19 +83,23 @@ function getServices() {
 function getService(id) {
   // console.log('service.model getService id', id);
   return new Promise((resolve, reject) => {
-    let sql = `SELECT * FROM servicos WHERE idservico = ${id}`;
-    mysql.getConnection((err, conn) => {
-      conn.query(sql, (err, result) => {
-        // console.log('service.model getService result', result);
-        console.log('service.model getService conn.query err', err);
-        conn.release();
-        if (err) {
-          reject(err);
-          return;
-        }
-        resolve(result);
+    try {
+      let sql = `SELECT * FROM servicos WHERE idservico = ${id}`;
+      mysql.getConnection((err, conn) => {
+        conn.query(sql, (err, result) => {
+          // console.log('service.model getService result', result);
+          console.log('service.model getService conn.query err', err);
+          conn.release();
+          if (err) {
+            reject(err);
+            return;
+          }
+          resolve(result);
+        });
       });
-    });
+    } catch (err) {
+      console.log('client.model getService catch err', err);
+    }
   });
 }
 
@@ -99,19 +111,23 @@ function getService(id) {
 function deleteService(id) {
   // console.log('service.model deleteService id', id);
   return new Promise((resolve, reject) => {
-    let sql = `DELETE FROM servicos WHERE idservico = ${id}`;
-    mysql.getConnection((err, conn) => {
-      conn.query(sql, (err, result) => {
-        // console.log('service.model deleteService result', result);
-        console.log('service.model deleteService conn.query err', err);
-        conn.release();
-        if (err) {
-          reject(err);
-          return;
-        }
-        resolve(result);
+    try {
+      let sql = `DELETE FROM servicos WHERE idservico = ${id}`;
+      mysql.getConnection((err, conn) => {
+        conn.query(sql, (err, result) => {
+          // console.log('service.model deleteService result', result);
+          console.log('service.model deleteService conn.query err', err);
+          conn.release();
+          if (err) {
+            reject(err);
+            return;
+          }
+          resolve(result);
+        });
       });
-    });
+    } catch (err) {
+      console.log('client.model deleteService catch err', err);
+    }
   });
 }
 
@@ -123,38 +139,42 @@ function deleteService(id) {
 function updateService(service) {
   // console.log('service.model updateService service', service);
   return new Promise((resolve, reject) => {
-    let sql = `UPDATE servicos SET 
-    tiposervico = '${service.tiposervico}', 
-    ambienteservico = '${service.ambienteservico}', 
-    dtcadservico = '${service.dtcadservico}', 
-    dtevento = '${service.dtevento}', 
-    preco = ${service.preco}, 
-    statusservico = '${service.statusservico}', 
-    enderecoevento = '${service.enderecoevento}', 
-    numeroendevento = '${service.numero}', 
-    bairroevento = '${service.bairro}', 
-    cepevento = '${service.cep}', 
-    cidadeevento = '${service.cidade}', 
-    nomebebe = '${service.nomebebe}', 
-    dtnascbebe = '${service.dtnascbebe}', 
-    nomecrianca = '${service.nomecrianca}', 
-    dtnasccrianca = '${service.dtnasccrianca}', 
-    idcliente = ${service.idcliente} 
-    WHERE idservico = ${service.idservico}`;
+    try {
+      let sql = `UPDATE servicos SET 
+        tiposervico = '${service.tiposervico}', 
+        ambienteservico = '${service.ambienteservico}', 
+        dtcadservico = '${service.dtcadservico}', 
+        dtevento = '${service.dtevento}', 
+        preco = ${service.preco}, 
+        statusservico = '${service.statusservico}', 
+        enderecoevento = '${service.enderecoevento}', 
+        numeroendevento = '${service.numero}', 
+        bairroevento = '${service.bairro}', 
+        cepevento = '${service.cep}', 
+        cidadeevento = '${service.cidade}', 
+        nomebebe = '${service.nomebebe}', 
+        dtnascbebe = '${service.dtnascbebe}', 
+        nomecrianca = '${service.nomecrianca}', 
+        dtnasccrianca = '${service.dtnasccrianca}', 
+        idcliente = ${service.idcliente} 
+        WHERE idservico = ${service.idservico}`;
 
-    // console.log('service.model updateService sql', sql);
-    mysql.getConnection((err, conn) => {
-      conn.query(sql, (err, result, field) => {
-        // console.log('service.model updateService result', result);
-        console.log('service.model updateService conn.query err', err);
-        conn.release();
-        if (err) {
-          reject(err);
-          return;
-        }
-        resolve(result);
+      // console.log('service.model updateService sql', sql);
+      mysql.getConnection((err, conn) => {
+        conn.query(sql, (err, result, field) => {
+          // console.log('service.model updateService result', result);
+          console.log('service.model updateService conn.query err', err);
+          conn.release();
+          if (err) {
+            reject(err);
+            return;
+          }
+          resolve(result);
+        });
       });
-    });
+    } catch (err) {
+      console.log('client.model updateService catch err', err);
+    }
   });
 }
 
