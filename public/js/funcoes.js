64bf73cc09a2db934ff20f708bcf41d26c3d827b
clebson.araujo <clebson.araujo@l5.com.br>
2024-04-12 09:08:42 -0300

    mudança na página de clientes e mais algumas outras coisas

--- public/js/funcoes.js
+++ public/js/funcoes.js
@@ -1,3 +1,27 @@
+/**
+ * Arquivo funcoes.js, responsvável por ações e estilizações e comportamento dos arquivos
+ * da pasta view e inc de modo geral.
+ */
+
+/** Eventos */
+$(document).ready(() => {
+
+    $(window).on("load", () => {
+        setTimeout(() => {
+            $(window).scrollTop(0);
+        }, 1);
+    });
+
+    /** Chama a função de abrir ou fechar modal */
+    window.onclick = function (event) {
+        if (event.target.className == "modal") {
+            abreOuFechaModal();
+        }
+    }
+});
+
+/** Funções */
+
 /** Abre ou fecha modal */
  function abreOuFechaModal() {
     if ($(".modal").css("display") == "block") {
@@ -8,67 +32,9 @@
     }
 }
 
-/** Chama a função de abrir ou fechar modal */
-window.onclick = function (event) {
-    if (event.target.className == "modal") {
-        abreOuFechaModal();
-    }
-}
-
-/** Limpa o formulário através do Id do formulário */
+/** Limpa o formulário */
 function limpaFormulario() {
     $("form").each (function() {
         this.reset();
     }); 
 }
-
-function abreMenu() {
-    $("main").css("opacity", "0.2");
-    $(".main-galery").css("opacity", "0.2");
-    $(".about").css("opacity", "0.2");
-    $(".main-blog").css("opacity", "0.2");
-    $(".nav-list li").css("opacity", "1");
-    $(".mobile-menu").addClass("active");
-}
-
-function fechaMenu() {
-    $("main").css("opacity", "1");
-    $(".main-galery").css("opacity", "1");
-    $(".about").css("opacity", "1");
-    $(".main-blog").css("opacity", "1");
-    $(".nav-list li").css("opacity", "0");
-    $(".mobile-menu").removeClass("active");
-}
-
-$(document).ready(() => {
-    $(".mobile-menu").click((i, el) => {
-        if ($(".mobile-menu").hasClass("active")) {
-            fechaMenu();
-        } else {
-            abreMenu();
-        }
-    });
-    // $(window).scrollTop(0);
-
-    $(window).on("load", () => {
-        setTimeout(() => {
-            $(window).scrollTop(0);
-        }, 1);
-    })
-});
-
-
-
-$(window).on("scroll", () => {
-    let header_fixo = {
-        height: "70px",
-        position: "fixed"
-    }
-    if ($(window).scrollTop() >= 136) {
-        $("header").css(header_fixo);
-
-    }
-})
-
-
-// $(".modalBtn").click(abreOuFechaModal);
