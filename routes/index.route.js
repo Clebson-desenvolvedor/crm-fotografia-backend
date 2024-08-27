d12a3fde8cba41b2cdb73c4115f0ec53b31da48f
clebson.araujo <clebson.araujo@l5.com.br>
2024-08-26 22:00:31 -0300

    banco atualizado e bloqueio nas rotas públicas

--- routes/index.route.js
+++ routes/index.route.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const indexController = require("../controllers/index.controller")
 
-router.get("/", indexController.getIndex);
-router.get("/galery", indexController.getGalery);
-router.get("/about", indexController.getAbout);
-router.get("/contact", indexController.getContact);
-router.get("/blog", indexController.getBlog);
+// router.get("/", indexController.getIndex);
+// router.get("/galery", indexController.getGalery);
+// router.get("/about", indexController.getAbout);
+// router.get("/contact", indexController.getContact);
+// router.get("/blog", indexController.getBlog);
 
 module.exports = router;
